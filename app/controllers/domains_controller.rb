60b3f351cec5e227f2362db7208149566b7572c5
Prelang Builder <builder@prelang.com>
2014-04-08 21:09:02 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,5 +1,5 @@
 class DomainsController < ApplicationController
-  before_action :set_domain, only: [:show, :edit, :update, :destroy]
+  before_action :set_domain, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /domains
   # GET /domains.json
